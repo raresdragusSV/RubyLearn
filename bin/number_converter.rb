144eb5b2689b9d390844088f61c3370f6b37ac3d
Rares Dragus <rares.dragus@softvision.ro>
2015-10-08 10:13:49 +0300

    Creating errors for the input!!! command

--- bin/number_converter.rb
+++ bin/number_converter.rb
@@ -2,6 +2,48 @@ require_relative '../lib/argument_processor'
 
 args = ARGV.dup
 
-argument_processor = ArgumentProcessor.new(args)
-argument_processor.run
+if args.size == 0
+  puts 'ERROR: This command must accept arguments!'
+  exit
+elsif args.size == 1
+  case args[0]
+  when '-r'
+    puts 'ERROR: You must supply a roman numeral!!!'
+    exit
+  when '-a'
+    puts 'ERROR: You must supply an arabic number!!!'
+    exit
+  when '-f'
+    puts 'ERROR: You must specify a text file!!!'
+    exit
+  else
+    puts 'ERROR: Wrong argument. Try -r, -a, -f, --list !!!'
+    exit
+  end
+elsif args.size == 2
+  case args[0]
+  when '--list'
+    puts 'ERROR: --list could not have another argument!!!'
+    exit
+  end
+elsif args.size > 2
+  case args[0]
+  when '-r'
+    puts 'ERROR: -r could not have more than two arguments!!!'
+    exit
+  when '-a'
+    puts 'ERROR: -a could not have more than two arguments!!!'
+    exit
+  when '-f'
+    puts 'ERROR: -f could not have more than two arguments!!!'
+    exit
+  else
+    argument_processor = ArgumentProcessor.new(args)
+    argument_processor.run
+  end
+end
+
+
+
+
 
